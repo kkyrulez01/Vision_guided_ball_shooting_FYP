34aedf8b9509e2234cbd0287e387781053efa864
kok yoong <kokyoongkang@gmail.com>
2026-01-31 18:00:23 +0800

    Allows add_HSV_filter to take in lower and upper bounds and apply the mask onto frame

--- src/fyp_wamv_project/fyp_wamv_project/HSV_filter.py
+++ src/fyp_wamv_project/fyp_wamv_project/HSV_filter.py
@@ -1,27 +1,34 @@
 import cv2
 import numpy as np
 
-def add_HSV_filter(frame):
+def add_HSV_filter(frame, lower, upper):
     # Convert BGR to HSV
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # Define lower and upper bounds for black color in HSV
-    lower = np.array([0, 0, 0])
-    upper = np.array([180, 255, 30])
 
     # Create a mask using defined bounds
     mask = cv2.inRange(frame_hsv, lower, upper)
 
-    cv2.imshow("Original frame", frame)
-    cv2.imshow("HSV frame", frame_hsv)
-    cv2.imshow("Mask", mask)
+    # Apply the mask to get the filtered image
+    filtered_frame_HSV = cv2.bitwise_and(frame, frame, mask=mask)
+    filtered_frame_BGR = cv2.cvtColor(filtered_frame_HSV, cv2.COLOR_HSV2BGR)
+    
+    # Display the results
+    # cv2.imshow("Original frame", frame)
+    # cv2.imshow("HSV frame", frame_hsv)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Filtered frame", filtered_frame)
+    # cv2.waitKey(0)
 
-    cv2.waitKey(0)
-
+    return filtered_frame_BGR
+    
 def main():
     # Load example image
-    image = cv2.imread('/home/kky/Pictures/Screenshots/test_image.png')
-    add_HSV_filter(image)
+    image = cv2.imread('/home/kky/fyp_ws/src/fyp_wamv_project/fyp_wamv_project/templates/small_target_template.png')
+    # Define lower and upper bound for black color
+    lower = np.array([0, 0, 0]) 
+    upper = np.array([180, 255, 30])
+    add_HSV_filter(image, lower, upper)
+
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
