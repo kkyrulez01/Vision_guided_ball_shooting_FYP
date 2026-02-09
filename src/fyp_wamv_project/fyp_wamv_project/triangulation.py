f2c3b6e0911bbe1ae4a579b986c3991e009a35e0
kok yoong <kokyoongkang@gmail.com>
2026-02-09 23:59:56 +0800

    Updated back_projection

--- src/fyp_wamv_project/fyp_wamv_project/triangulation.py
+++ src/fyp_wamv_project/fyp_wamv_project/triangulation.py
@@ -15,7 +15,19 @@ def calculate_focal_length(image_width_px, HFOV_rad):
     f_px = (image_width_px) / (2 * np.tan(HFOV_rad / 2))
     return f_px
 
-def triangulate_points(u, v, disparity_map, frame, baseline, focal_length):
+def triangulation(u, v, disparity_map, baseline, focal_length):
+    """
+    Calculate Z-value (depth) of a pixel using focal length, baseline and disparity.
+
+    Arguments:
+        u, v: Pixel coordinate in disparity map
+        disparity_map : Disparity map
+        baseline: The baseline or the distance between the two cameras.
+        focal_length: Focal length of camera.
+    
+    Returns:
+        The Z value of a pixel.
+    """
     f = focal_length # in pixels
     B = baseline # in meters
 
@@ -23,18 +35,21 @@ def triangulate_points(u, v, disparity_map, frame, baseline, focal_length):
     disparity = disparity_map[v, u] # Dont need to divide by 16 since already done in depth map computation
     Z = (f * B) / disparity # Depth of the point in meters
 
-    if Z > 0: 
+    return Z
+
+def back_projection(Z, u, v, c_x, c_y, f_x, f_y, frame):
+    if Z > 0:
+        # Calculate X and Y using Z value
+        X = ((u - c_x) * Z / f_x)
+        Y = ((v- c_y) * Z / f_y)
+
         # Draw a circle in the top left of bounding boxes
         cv2.circle(frame, (u, v), 5, (255,0,0), -1)
         # Label the Z value of center point
-        cv2.putText(frame, f"Z: {Z:.2f}m", (u + 10, v - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
-
-    return Z
-
-def back_projection(Z, u, v, c_x, c_y, f_x, f_y):
-    X = ((u - c_x) * Z / f_x)
-    Y = ((v- c_y) * Z / f_y)
-
-    return X,Y
+        cv2.putText(frame, f"Z: {Z:.2f}m, X: {X:.2f}m, Y: {Y:.2f}m",
+                    (u + 10, v - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        
+    return frame
+        
     
 
