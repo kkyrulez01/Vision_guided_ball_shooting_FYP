4c059564d46e658057b04e83d7187e64a5b71b98
kok yoong <kokyoongkang@gmail.com>
2026-02-20 01:23:23 +0800

    Tweaked wls_filter parameters

--- src/fyp_wamv_project/fyp_wamv_project/depth_map.py
+++ src/fyp_wamv_project/fyp_wamv_project/depth_map.py
@@ -9,7 +9,7 @@ class DepthMap:
     
     def compute_depth_mapBM(self):
         numDisparities_factor = 8 # Adjust this
-        blockSize = 5 # Must be odd and more than 3 (Default is 21)
+        blockSize = 7 # Must be odd and more than 3 (Default is 21)
 
         # Create left and right matchers using StereoBM algorithm
         left_matcher = cv2.StereoBM_create(numDisparities= 16 * numDisparities_factor, blockSize=blockSize)
@@ -17,13 +17,14 @@ class DepthMap:
 
         # Create the WLS filter
         wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
-        wls_filter.setLambda(8000) # Smoothing strength
+        wls_filter.setLambda(400) # Smoothing strength
         wls_filter.setSigmaColor(1.5) # How much to respect RGB edges
 
         # Compute disparity maps
-        disp_l = left_matcher.compute(self.left_frame, self.right_frame).astype(np.float32) / 16.0 # Divide by 16 to get actual disparity values
-        disp_r = right_matcher.compute(self.right_frame, self.left_frame).astype(np.float32) / 16.0
+        disp_l = left_matcher.compute(self.left_frame, self.right_frame) 
+        disp_r = right_matcher.compute(self.right_frame, self.left_frame)
         filtered_disp_BM = wls_filter.filter(disp_l, self.left_frame, disparity_map_right=disp_r)
+        filtered_disp_BM = filtered_disp_BM.astype(np.float32) / 16.0 # Divide by 16 to get actual disparity values
         return filtered_disp_BM
 
     def compute_depth_mapSGBM(self):
@@ -44,8 +45,8 @@ class DepthMap:
 
         # Create the WLS filter
         wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
-        wls_filter.setLambda(8000) # Smoothing strength
-        wls_filter.setSigmaColor(1.5) # How much to respect RGB edges
+        wls_filter.setLambda(400) # Smoothing strength
+        wls_filter.setSigmaColor(2) # How much to respect RGB edges
 
         # Compute disparity map
         disp_l = left_matcher.compute(self.left_frame, self.right_frame).astype(np.float32) / 16.0 # Divide by 16 to get actual disparity values
@@ -69,6 +70,12 @@ def main():
     right_frame = cv2.imread('src/fyp_wamv_project/fyp_wamv_project/example_images/right_camera_feed.png')
     left_frame = cv2.cvtColor(left_frame, cv2.COLOR_BGR2GRAY)
     right_frame = cv2.cvtColor(right_frame, cv2.COLOR_BGR2GRAY)
+
+    # # Without filter
+    # stereo = cv2.StereoBM_create(numDisparities=16 * 8, blockSize=7)
+    # disparity = stereo.compute(left_frame, right_frame).astype(np.float32) / 16.0
+    # plt.imshow(disparity, 'gray')
+    # plt.show()
     # depth_map = DepthMap(left_frame,right_frame)
     
     # depth_map.compute_depth_mapBM()
@@ -76,18 +83,31 @@ def main():
     # depth_map.plot_images()
 
     # Test WLS filter
-    left_matcher = cv2.StereoBM_create(numDisparities=16 * 8, blockSize=5)
+    left_matcher = cv2.StereoBM_create(numDisparities=16 * 8, blockSize=7)
+    # left_matcher = cv2.StereoSGBM_create(
+    #         minDisparity=16, # Minimum disparity value
+    #         numDisparities=16 * 10, # Max disparity - min disparity, must be multiple of 16
+    #         blockSize=5, # Must be an odd number >=1, Usually between 3-11
+    #         P1= 8 * 3 * 5**2, # Controls disparity smoothness, penalty when disparity difference is 1
+    #         P2= 32 * 3 * 5**2, # Controls smoothness, penalty when disparity difference > 1. Rule of thumb: P2 > P1
+    #         disp12MaxDiff=1,
+    #         uniquenessRatio=20,
+    #         speckleWindowSize=200,
+    #         speckleRange=2,
+    #         mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY,
+    #     )
     right_matcher = cv2.ximgproc.createRightMatcher(left_matcher) # Required for WLS filter
     # Create the WLS filter
     wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
-    wls_filter.setLambda(8000) # Smoothing strength
-    wls_filter.setSigmaColor(1) # How much to respect RGB edges
+    wls_filter.setLambda(800) # Smoothing strength
+    wls_filter.setSigmaColor(1.2) # How much to respect RGB edges
     # Compute disparity maps
     disp_l = left_matcher.compute(left_frame, right_frame).astype(np.float32) / 16.0
     disp_r = right_matcher.compute(right_frame, left_frame).astype(np.float32) / 16.0
     filtered_disp = wls_filter.filter(disp_l, left_frame, disparity_map_right=disp_r)
+    
     plt.imshow(filtered_disp, 'gray')
     plt.show()
-    
+
 if __name__ == "__main__":
     main()
