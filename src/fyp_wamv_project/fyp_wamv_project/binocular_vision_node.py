89216245aae9f16614dae0f7c71685c389a468d1
kok yoong <kokyoongkang@gmail.com>
2026-03-12 02:07:08 +0800

    Now allows user to select mode

--- src/fyp_wamv_project/fyp_wamv_project/binocular_vision_node.py
+++ src/fyp_wamv_project/fyp_wamv_project/binocular_vision_node.py
@@ -6,15 +6,20 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 import message_filters
+import argparse
+import sys
 
 from fyp_wamv_project.binocular_vision_engine import BaseDetector, TemplateMatcher, ColorEdgeDetector
 from fyp_wamv_project.shape_detection import detect_shape
 from fyp_wamv_project.HSV_filter import add_HSV_filter
 
 class BinocularVision(Node):
-    def __init__(self):
+    def __init__(self, mode):
         super().__init__('binocular_vision')
         self.bridge = CvBridge()
+
+        # Store mode argument (Default: 2)
+        self.mode = mode
 
         # Create subscribers to left and right camera
         self.left_image_sub = message_filters.Subscriber(self, Image, '/wamv/sensors/cameras/front_left_camera_sensor/image_raw',)
@@ -35,19 +40,39 @@ class BinocularVision(Node):
         self.right_camera_image = self.bridge.imgmsg_to_cv2(right_image_msg, desired_encoding='bgr8')
         self.get_logger().info('Received images from both cameras')
 
-        # Template matching
-        templates = ['/home/kky/fyp_ws/src/fyp_wamv_project/fyp_wamv_project/templates/big_target_template.png',]
-                    
+        # # Uncomment to show left and right camera frames
+        # combined_frame = np.hstack((self.left_camera_image, self.right_camera_image))
+        # h, w = self.left_camera_image.shape[:2]
+        # cv2.line(combined_frame, (w, 0), (w, h), (0, 0, 0), 1) # Black line to seperate the 2 camera frames
+        # cv2.imshow('Dual camera feed', combined_frame)
+        # cv2.waitKey(1)
 
+        # Template matching
+        templates = ['src/fyp_wamv_project/fyp_wamv_project/templates/small_target_template_1.png',
+                    'src/fyp_wamv_project/fyp_wamv_project/templates/small_target_template_2.png',
+                    'src/fyp_wamv_project/fyp_wamv_project/templates/small_target_template_3.png']
+                    
         # Focal length and baseline in meters
         baseline = 0.2 # in meters
         
         processor1 = TemplateMatcher(self.left_camera_image, self.right_camera_image, templates)
         processor2 = ColorEdgeDetector(self.left_camera_image, self.right_camera_image, baseline)
         
-        # Choose approach
-        # disparity_1, frame_1 = processor1.process_frames()
-        disparity_2, filtered_frame_2, targets = processor2.process_frames()
+        # Choose approach depending on arguments, default mode is 2
+        # Template matching
+        if self.mode == 1:
+            disparity_1, filtered_frame_1 = processor1.process_frames()
+
+            cv2.imshow("Template matching", filtered_frame_1)
+            cv2.waitKey(1)
+
+        # Color edge detection
+        else:
+            disparity_2, filtered_frame_2, targets = processor2.process_frames()
+
+            # Display the processed frame
+            cv2.imshow("Color Edge detection", filtered_frame_2)
+            cv2.waitKey(1)
 
         # # Display the processed_frame
         # cv2.imshow("Left_camera_feed", filtered_left_frame)
@@ -55,9 +80,7 @@ class BinocularVision(Node):
         # cv2.imshow("Depth_map", disparity)
         # cv2.imshow("Filtered Left frame", filtered_left_frame)
         # cv2.imshow("Template matching", frame_1)
-        cv2.imshow("Color Edge detection", filtered_frame_2)
-        cv2.waitKey(1)
-
+        
         msg = PoseArray()
         for target in targets:
             if target[0] is not None and target[1] is not None and target[2] is not None:
@@ -77,8 +100,16 @@ class BinocularVision(Node):
         self.targets_publisher.publish(msg)
 
 def main(args=None):
+    # Create a parser to allow user to choose which method to use
+    # 1: Template matching 2:  Color Edge detection
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=int, default = 2)
+    # ROS2 passes hidden arguments that argparse doesn't recognize
+    # parse_known_args() captures only our arguments and ignores the ROS2 arguments
+    parsed_args, unknown = parser.parse_known_args() 
+
     rclpy.init(args=args)
-    binocular_vision = BinocularVision()
+    binocular_vision = BinocularVision(mode=parsed_args.mode)
     rclpy.spin(binocular_vision)
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
