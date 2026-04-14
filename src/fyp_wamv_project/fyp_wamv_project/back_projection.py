8d967cd53efa2a5d43dd1de897f922abae3d2ade
kok yoong <kokyoongkang@gmail.com>
2026-04-14 20:01:43 +0800

    Renamed files

---
+++ src/fyp_wamv_project/fyp_wamv_project/back_projection.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+
+def calculate_focal_length(image_width_px, HFOV_rad):
+    """
+    Calculates the focal length of the camera in pixels.
+
+    Arguments:
+        image_width_px (int): Width of the image in pixels
+        HFOV_rad (float) : Horizontal field-of-view of the camera in terms of radians.
+
+    Returns:
+        f_px (float): The focal length of the camera in pixels. 
+    """
+    f_px = (image_width_px) / (2 * np.tan(HFOV_rad / 2))
+    return f_px
+
+def triangulation(u, v, disparity_map, baseline, focal_length):
+    """
+    Calculate Z-value (depth) of a pixel using focal length, baseline and disparity.
+
+    Arguments:
+        u, v: Pixel coordinate in disparity map
+        disparity_map : Disparity map
+        baseline: The baseline or the distance between the two cameras.
+        focal_length: Focal length of camera.
+    
+    Returns:
+        The Z value of a pixel.
+    """
+    f = focal_length # in pixels
+    B = baseline # in meters
+
+    # Disparity map is a matrix, so to access a pixel at (u,v) we use disparity_map[v, u]
+    disparity = disparity_map[v, u] # Dont need to divide by 16 since already done in depth map computation
+    if disparity > 0:
+        Z = (f * B) / disparity # Depth of the point in meters
+        return Z
+
+def back_projection(Z, u, v, c_x, c_y, f_x, f_y, frame, alpha=np.radians(-15)):
+    if Z is not None and Z > 0:
+        # Calculate X and Y using Z value
+        X = ((u - c_x) * Z / f_x)
+        Y = ((v - c_y) * Z / f_y)
+
+        # Camera is tilted at 15 degrees, need to account for that by rotating about X axis
+        actual_Z = Z * np.cos(alpha) - Y * np.sin(alpha)
+        actual_Y = Z * np.sin(alpha) + Y * np.cos(alpha)
+
+        # Draw a circle in the centre of bounding boxes
+        cv2.circle(frame, (u, v), 5, (255,0,0), -1)
+        # Label the Z,X,Y value of centre point
+        size, _ = cv2.getTextSize(f"Z: {actual_Z:.2f}m,X: {X:.2f}m,Y: {actual_Y:.2f}m", cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
+        text_w, text_h = size
+        cv2.putText(frame, f"Z: {actual_Z:.2f}m,X: {X:.2f}m,Y: {actual_Y:.2f}m",
+                    (u - text_w // 2, v - text_h), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,255,0), 1)
+
+        return X, actual_Y, actual_Z, frame
+
+    else:
+        return None, None, None, frame
+
+def back_projection_no_drawing(Z, u, v ,c_x, c_y, f_x, f_y, alpha=np.radians(-15)):
+    if Z is not None and Z > 0:
+        # Calculate X and Y using Z value
+        X = ((u - c_x) * Z / f_x)
+        Y = ((v - c_y) * Z / f_y)
+
+        # Camera is tilted at 15 degrees, need to account for that by rotating about X axis
+        actual_Z = Z * np.cos(alpha) - Y * np.sin(alpha)
+        actual_Y = Z * np.sin(alpha) + Y * np.cos(alpha)
+
+        return X, actual_Y, actual_Z
+        
+    else:
+        return None, None, None
+        
+    
+
---
+++ src/fyp_wamv_project/fyp_wamv_project/edge_detection.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from fyp_wamv_project.HSV_filter import add_HSV_filter
+
+def detect_target(frame):
+    gray_frame = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY) # Convert to grayscale first
+
+    # # Trackbars to adjust Canny edge detection minVal, maxVal
+    # cv2.namedWindow('Parameters')
+    # cv2.createTrackbar('minVal', 'Parameters', 0, 500, lambda x: None)
+    # cv2.createTrackbar('maxVal', 'Parameters', 0, 500, lambda x: None)
+
+    # # Trackbar to adjust contour area threshold
+    # cv2.createTrackbar('Area', 'Parameters', 0, 1000, lambda x: None)
+
+    # Use Canny edge detection, returns a binary image with thin edges
+    # minVal = cv2.getTrackbarPos('minVal', 'Parameters')
+    # maxVal = cv2.getTrackbarPos('maxVal', 'Parameters')
+    edges = cv2.Canny(gray_frame, 50, 150) # Input image, minVal, maxVal
+
+    # Get contours
+    contours, hierachy = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, 
+                                          cv2.CHAIN_APPROX_SIMPLE) # Source image, contour retrieval mode, contour approximation method
+
+    # Only draw contours that are large enough (avoid noise)
+    contours_frame = frame.copy()
+    # Store the centre coordinates and top left of the shape
+    targets_centre = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        # areaThreshold = cv2.getTrackbarPos('Area', 'Parameters')
+        if area > 500:
+            # cv2.drawContours(contours_frame, cnt, -1, (0,255,0), 3) # -1 to draw all contours, color, thickness
+            # peri = cv2.arcLength(cnt, True) # True indicates that the contour is closed
+            # approx = cv2.approxPolyDP(cnt, 0.02 * peri, True) 
+            # x, y, w, h = cv2.boundingRect(approx)
+            # cv2.rectangle(contours_frame, (x,y), (x+w, y+h), (255,0,255), 2)
+            # targets_centre.append((x+w//2, y+h//2)) # Append the centre of the shape
+
+            rect = cv2.minAreaRect(cnt)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(contours_frame, [box], 0, (255,0,255), 2)
+            center_x = int(rect[0][0])
+            center_y = int(rect[0][1])
+            targets_centre.append((center_x, center_y))
+
+    # # Plot image
+    # plt.figure(figsize=(15,10))
+    # plt.subplot(1,3,1)
+    # plt.imshow(frame, cmap='gray') # Show original image
+    # plt.title('Before shape detection')
+    # plt.axis('off')
+
+    # plt.subplot(1,3,2)
+    # plt.imshow(edges, cmap='gray') # Show edges detected
+    # plt.title('After edge detection')
+    # plt.axis('off')
+
+    # plt.subplot(1,3,3)
+    # plt.imshow(contours_frame)
+    # plt.title('Bounding boxes drawn')
+    # plt.axis('off')
+
+    # plt.show()
+
+    return contours_frame,shapes_centre
+
+def main():
+    frame = cv2.imread('src/fyp_wamv_project/fyp_wamv_project/example_images/left_camera_feed.png')
+    lower = np.array([0, 0, 0]) 
+    upper = np.array([180, 255, 30])
+    frame_HSV = add_HSV_filter(frame, lower,upper)
+
+    contours_frame, shapes_centre = detect_shape(frame_HSV)
+    print(shapes_centre)
+
+if __name__=="__main__":
+    main()
+
